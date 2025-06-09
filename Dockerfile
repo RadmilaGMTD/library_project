517f8afb503706a107d654d5dcb7e49b79f1873d
Radmila Gimatdinova <raf1507@mail.ru>
2025-06-09 05:00:41 +0400

    update 2

--- Dockerfile
+++ Dockerfile
@@ -25,3 +25,5 @@ RUN mkdir -p /app/staticfiles && chmod -R 755 /app/staticfiles
 
 # Пробрасываем порт, который будет использовать Django
 EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- config/settings.py
+++ config/settings.py
@@ -83,16 +83,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DATABASE_HOST", "localhost"),
-        "PORT": os.getenv("DATABASE_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
